ca10c9377084353f4f00a94af3ad6be95edcf7b9
krbundy <kenneth.bundy@maine.edu>
2020-10-31 23:42:09 -0400

    Added stubs for zoom grader

---
+++ js/zoomGrader.js
@@ -0,0 +1,30 @@
+function loadfiles(files){
+    //loads all the files from the webform and returns them as a JS Object
+
+}
+
+function cleanZoomData(names, emails, attendance){
+// Cleans the data from zoom, by:
+// 1. removes extra characters, moves everything to lower case
+// 2. combines duplicate entries.
+}
+
+function createMasterList(list, tokens){
+    // returns master list from an array of names.  Formatted for a reduce operation.
+}
+
+function colateAttendance(masterList, names, emails, attendance){
+    // Takes all of the information from the loaded file, and then adds them to the master list
+
+}
+
+function run(files){
+    //The function that is called on clicking the run button
+    var data  = loadfiles();
+    var cleaned_data  = cleanZoomData(data);
+    let masterList = [];
+    masterlist = 
+
+}
+
+//helper functions
